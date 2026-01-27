f513b5583af5a17ac5f6da4d9dddcbfe541c9d18
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-01-27 22:05:33 +0530

    add new

---
+++ 1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+//3 number largest using nested if
+
+int main(){
+int a=10,b=20,c=30;
+
+if(a>b){
+    cout<<"A is big"<<endl;
+}
+else if(b>c){
+    cout<<"B is big"<<endl;
+}
+else{
+    cout<<"C is big"<<endl;
+}
+
+
+}
