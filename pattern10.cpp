05a7706e080dbc6cd07d1bcf8f8003e7f49cbb6b
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-03-27 21:47:23 +0530

    add pattern code

---
+++ pattern10.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+/*
+*
+**
+***
+****
+*****
+*****
+****
+***
+**
+*
+
+*/
+
+class solution{
+    public:
+    void pattern10(int n){
+          for(int i = 0; i <n; i++) {          
+            for(int j = 0; j <i; j++) {      
+                cout <<"*";
+            }
+            cout << endl;
+        }
+         for(int i=n;i>0;i--){
+            for(int j=0;j<i;j++){
+                cout<<"*";
+            }
+            cout<<endl;
+
+        }
+
+       
+    }
+};
+
+int main(){
+     solution s1;
+     s1.pattern10(5);
+
+}
