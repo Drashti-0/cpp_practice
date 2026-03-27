4829d48207aeb8cfcf040e55e3cbe3bcc1227949
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-03-27 21:48:15 +0530

    add new pattern code

---
+++ pattern11.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+/*
+
+1
+01
+101
+0101
+10101
+
+*/
+
+class solution
+{
+public:
+    void pattern11(int n){
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                if((i+j)%2==0){
+                    cout<<"1";
+                }
+                else{
+                    cout<<"0";
+                }
+            }
+            cout<<endl;
+        }
+    }
+    
+};
+
+int main()
+{
+    solution s1;
+    s1.pattern11(5);
+}
+
