4594996fb95976b4f4d93bd5d968174b345dd4fb
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-04-01 21:51:00 +0530

    add patttern practice

---
+++ pattern22.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+
+/*
+
+4 4 4 4 4 4 4 
+4 3 3 3 3 3 4
+4 3 2 2 2 3 4
+4 3 2 1 2 3 4
+4 3 2 2 2 3 4
+4 3 3 3 3 3 4
+4 4 4 4 4 4 4
+
+*/
+
+int main() {
+    int n = 4;
+    int size = 2*n - 1;
+
+    for(int i=0; i<size; i++){
+        for(int j=0; j<size; j++){
+
+            int value = n;
+
+            
+            if(i < value) value = i;
+
+            
+            if(j < value) value = j;
+
+            
+            if(size-1-i < value) value = size-1-i;
+
+            
+            if(size-1-j < value) value = size-1-j;
+
+            cout << (n - value) << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
