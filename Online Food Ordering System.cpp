d6be9b1d076f1e62d2662bbd15ea4b5e47e6350b
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-04-05 22:20:12 +0530

    add cpp file

--- Online Food Ordering System.cpp
+++ Online Food Ordering System.cpp
@@ -1,23 +1,42 @@
 #include<iostream>
 using namespace std;
 
-// Pizza
-int total(int price, int quantity){
-    return price * quantity;
-}
+class food{
+    private:
 
-// Burger
-int total(int price, int quantity, int packaging){
-    return (price * quantity) + packaging;
-}
+    int price;
+    int quntt;
+    int pcharge;
+    int totalp;
+
+    public:
+
+    void fooditeam(int p){
+        cout<<"Enter your pizza quantity: "<<endl;
+        cin>>quntt;
+
+        totalp = p*quntt;
+
+        cout<<"total price of pizza: "<<totalp<<endl;
+    }
+
+    void fooditeam(int price,int pcharge){
+        cout<<"Enter your Burgerquntity: "<<endl;
+        cin>>quntt;
+
+        totalp= (price*quntt)+pcharge;
+
+        cout<<"total price of burger: "<<totalp<<endl;
+    }
+
+
+};
 
 int main(){
+    food p,b;
 
-    int pizzaTotal = total(200, 2); 
-    int burgerTotal = total(100, 3, 20);
-
-    cout<<"Pizza Total Price: "<<pizzaTotal<<endl;
-    cout<<"Burger Total Price: "<<burgerTotal<<endl;
-
-    return 0;
+    p.fooditeam(200);
+    b.fooditeam(70,50);
 }
+
+
