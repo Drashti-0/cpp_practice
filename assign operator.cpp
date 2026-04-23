7a8a9f229f5fd370469f22d8fb1b7fd0dee7c6cc
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-04-23 23:49:54 +0530

    assign oparator  practice

---
+++ assign operator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //string str1={"c++ with programming",5,7};
+
+    string str1;
+    str1.assign("c++ with programming",5,7);
+
+    cout<<str1;
+    return 0;
+}
