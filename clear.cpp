a970f45988497bf22c23afe67828b2f0a20d8507
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-04-24 22:01:27 +0530

    add basic syntex practice

---
+++ clear.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<sstream>
+using namespace std;
+
+int main() {
+   string s;
+
+   cout<<"Enter sytring:";
+   cin>>s;
+
+   cout<<s<<endl;
+s.clear();
+
+cout<<s<<endl;
+
+cout<<s<<endl;
+
+cout<<s<<endl;
+
+cout<<s<<endl;
+
+   return 0;
+}
