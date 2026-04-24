3faed53e480bccfb5397e4ec06fcba1a82d0a176
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-04-24 22:02:52 +0530

    add new

---
+++ insert.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    
+
+    string str1=("Drashti  ");
+    str1.insert(7," HELLOWWW");
+
+    cout<<str1;
+    return 0;
+}
