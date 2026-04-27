34a14083e8e25546c779b1ddad2b248f3e553b36
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-04-27 22:20:06 +0530

    add new cpp file

---
+++ string to anuthor datatype.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    
+       string str1=("55");
+       string str2=("58.69");
+       int a=stoi(str1);
+       float b=stof(str2);
+
+       cout<<a<<endl<<b<<endl;
+
+    return 0;
+}
