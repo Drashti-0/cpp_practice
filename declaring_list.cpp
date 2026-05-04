f746ec50846873b2743da8c103299c94bd34c2f5
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-05-04 19:54:52 +0530

    add new cpp file

---
+++ declaring_list.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<list>
+using namespace std;
+
+int main() {
+    list<int>lst1;
+    list<int>lst2(5);
+    list<int>lst3(5,100);
+    list<int>lst4={1,2,3,4,5};
+
+
+
+
+    return 0;
+}
