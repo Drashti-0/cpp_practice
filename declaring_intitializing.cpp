577f8b51861c3cf6b93303b1de99f25562448063
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-05-04 19:54:23 +0530

    add cpp

---
+++ declaring_intitializing.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<deque>
+using namespace std;
+
+int main() {
+    deque<int>dq1;
+    deque<int>dq2(5);
+    deque<int>dq3(3,100);
+    deque<int>dq4={1,2,3,4,5};
+
+  
+}
