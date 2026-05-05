a53dbcb284033e663c165c00fbd86840b6ad2106
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-05-05 21:45:59 +0530

    add new

---
+++ function overloading templets.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int add(int a,int b){
+   return a+b;
+}
+
+float add(float a,float b){
+    return a+b;
+}
+
+
+int main() {
+
+    cout<<add(2,3)<<endl;
+    cout<<add(5.60f ,2.89f )<<endl;
+    
+    return 0;
+}
