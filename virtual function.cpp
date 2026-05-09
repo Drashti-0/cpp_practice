77997a9754f6a4064734a95eb7f14b3d91a6b24a
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-05-09 19:39:09 +0530

    add new cpp file

---
+++ virtual function.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class base{
+    public:
+    virtual void show(){
+
+        cout<<"Base\n";
+        }
+};
+
+class derived: public base{
+    public:
+    void show(){
+        cout<<"Derived\n";                                           
+    }
+
+};
+
+int main() {
+
+base* b;
+derived d;
+
+b=&d;
+
+b->show();
+    
+    return 0;
+}
+
+
+//output:::derived
