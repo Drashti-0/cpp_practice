dfa6fa1c6770bdbcbb6134ebe8561e6532043d87
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-05-09 19:30:33 +0530

    add new cpp programm

---
+++ Pointer to Object & this Pointer.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class test{
+    public:
+
+    int x;
+
+    void set(int x){
+        this->x =x;
+    }
+    void show() {
+        cout << x;
+    }
+};
+
+int main() {
+    test obj;
+    test * ptr =&obj;
+    //pointer to object
+
+    ptr->set(10);
+    ptr->show();
+    
+    return 0;
+}
