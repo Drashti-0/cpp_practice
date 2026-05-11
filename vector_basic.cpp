d7b5a1e8bd541b3e575b17d2969198350d7cdc3a
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-05-11 21:29:49 +0530

    cpp p[ractice

---
+++ vector_basic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+    vector<int>vec1;
+    vector<int>vec2(5);
+    vector<int>vec3(5,100);
+    vector<int>vec4={1,2,3,4,5};
+
+}
+
