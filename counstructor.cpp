6e36f26aabeba7dbccb01d19558a62be90317565
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-05-28 21:59:21 +0530

    add new file

---
+++ counstructor.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class C{
+    public:
+    string name;
+    C(){    
+    name="Drashti";
+    }
+};
+
+
+
+int main() {
+    C c1;
+   cout<<c1.name;   
+    return 0;
+}
